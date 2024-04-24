1abe60feea42b1e2d11792c1af84f7fe245e5832
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-04-24 14:30:37 +0700

    Initial commit
    Initial Commit for this project

---
+++ src/main/java/fit/se2/APlusBook/APlusBookApplication.java
@@ -0,0 +1,13 @@
+package fit.se2.APlusBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APlusBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APlusBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fit/se2/APlusBook/APlusBookApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.se2.APlusBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APlusBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
