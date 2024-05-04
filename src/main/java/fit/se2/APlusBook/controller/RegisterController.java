9e6449aa19a16e08bedc49614967ff5a37cc1e04
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-05-04 14:22:16 +0700

    Add login and register UI

--- src/main/java/fit/se2/APlusBook/controller/RegisterController.java
+++ src/main/java/fit/se2/APlusBook/controller/RegisterController.java
@@ -9,13 +9,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
     @Autowired
     private UserRepository userRepository;
-
+    @GetMapping("/log-in")
+    public String login() {
+        return "Authentication/login";
+    }
     @PostMapping("/register")
     public String registerHandle(Model model, @Valid UserSimple ut, BindingResult result) {
         if (result.hasErrors()) {
