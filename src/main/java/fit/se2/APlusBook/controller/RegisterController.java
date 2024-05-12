1e28d216575b32722deac7c60e8e48b740e55c51
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-05-12 11:36:32 +0700

    Get some code from be_QuocTran branch but it does not work

--- src/main/java/fit/se2/APlusBook/controller/RegisterController.java
+++ src/main/java/fit/se2/APlusBook/controller/RegisterController.java
@@ -1,10 +1,13 @@
 package fit.se2.APlusBook.controller;
 
+import fit.se2.APlusBook.dto.UserDto;
 import fit.se2.APlusBook.model.Category;
 import fit.se2.APlusBook.model.User;
 import fit.se2.APlusBook.model.UserSimple;
 import fit.se2.APlusBook.repository.CategoryRepository;
 import fit.se2.APlusBook.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +17,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -37,16 +42,24 @@ public class RegisterController {
     public String register() {
         return "Authentication/register";
     }
-    @PostMapping("/register")
+    @PostMapping("/register-process")
     public String registerHandle(Model model, @Valid UserSimple ut, BindingResult result) {
         if (result.hasErrors()) {
             model.addAttribute("user", ut);
-            return "register";
+            return "Authentication/register";
         } else {
             userRepository.save(new User(ut, new BCryptPasswordEncoder(4)));
             model.addAttribute("user", new UserSimple());
             model.addAttribute("success", true);
-            return "register";
+            return "Authentication/register";
         }
+    }
+    @RequestMapping(value = {"/log-in"})
+    public String logIn(final Model model,
+                        final HttpServletRequest request,
+                        final HttpServletResponse response)
+            throws IOException {
+
+        return "Authentication/login";
     }
 }
