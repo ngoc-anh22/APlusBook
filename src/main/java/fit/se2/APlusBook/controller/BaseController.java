febe7e0737cc3c484d224ec31dcf1122d058495e
QuocTran3112 <hakuhatsuma1504@gmail.com>
2024-05-15 22:33:57 +0700

    Account detail

---
+++ src/main/java/fit/se2/APlusBook/controller/BaseController.java
@@ -0,0 +1,17 @@
+package fit.se2.APlusBook.controller;
+
+import fit.se2.APlusBook.model.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+public abstract class BaseController {
+    @ModelAttribute("userLogined")
+    public User getUserLogined() {
+        Object userLogined = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (userLogined != null && userLogined instanceof UserDetails) {
+            return (User) userLogined;
+        }
+        return new User();
+    }
+}
